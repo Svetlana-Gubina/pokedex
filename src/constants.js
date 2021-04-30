95bec557a774c6c0ccf085201f3eab8f3f5ffd81
Svetlana Terenteva <gubinas83@gmail.com>
2021-05-01 00:41:15 +0300

    feat: add page-not-found component

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const AppRoute = {
+    // LOGIN: `/login`,
+    // LOGOUT: `/logout`,
+    ROOT: `/`,
+    POKEMON: `/pokemon/:id`,
+    CATCHED: `/catched`,
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
+import browserHistory from './browser-history';
+import {Router} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router history={browserHistory}>
+        <App />
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
